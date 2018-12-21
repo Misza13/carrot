6d19341353562dea376c7f2b813e7f1d15d97969
Misza <misza@misza.net>
2018-12-22 00:25:05 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='carrot_mc',
-    version='0.3',
+    version='0.3.1',
     description='Command-line Minecraft mod manager',
     long_description=long_description,
     long_description_content_type='text/markdown',
