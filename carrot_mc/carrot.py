ca42252fd542ec6e74ffb5c1bccd9ab553457f33
Misza <misza@misza.net>
2018-12-22 13:07:45 +0100

    Refactor: split run() method into several

--- carrot_mc/carrot.py
+++ carrot_mc/carrot.py
@@ -152,113 +152,126 @@ class InstallationManager:
     def run(self, carrot: CarrotModel, args):
         while not self.fetch_q.empty():
             req = self.fetch_q.get()
-
-            print(f'Checking mod {colorify(str(req.mod_key), WHITE+BRIGHT)}... ', end='')
-
-            mod_info = self.backend.get_mod_info(req.mod_key)
-
-            print(colorify(mod_info.name + '... ', WHITE+BRIGHT), end='')
-
-            mod_info.file = self.backend.get_newest_file_info(req.mod_key, req.mc_version, req.channel)
-
-            current_mod = find_mod_by_key(carrot.mods, mod_info.key)
-
-            # TODO: Handling of "sticky" channel
-
-            if not current_mod:
-                print('New mod. ', end='')
-
-                proceed = True
-
-            elif current_mod.file.id < mod_info.file.id:
-                if args.upgrade:
-                    print('Mod already installed, found new version and will upgrade because it\'s allowed. ', end='')
-
-                    proceed = True
-
-                else:
-                    print(f'A newer file was found but upgrades are disabled by default. Use the {colorify("--upgrade", RED+BRIGHT)} option if this should be allowed. ', end='')
-
-                    if req.dependency:
-                        print(f'\n{colorify("NOTE", RED+BRIGHT)}: Because this is a dependency, it will {colorify("not", BRIGHT)} be re-checked if you re-run last install command. Use "{colorify("carrot update " + mod_info.key, YELLOW+BRIGHT)}" to do it explicitly. ', end='')
-
-                    proceed = False
-
-            elif current_mod.file.id == mod_info.file.id:
-                print('Already at newest version. ', end='')
-
-                proceed = False
-
-            else:
-                if args.downgrade:
-                    print('Mod already installed, found older version and will downgrade because it\'s allowed. ', end='')
-
-                    proceed = True
-                else:
-                    print(f'An older file was found but downgrades are disabled by default. Use the {colorify("--downgrade", RED+BRIGHT)} option if this was intended. ', end='')
-
-                    proceed = False
-
-            if proceed:
-                self.download_q.put(DownloadRequest(
-                    mod_info=mod_info,
-                    dependency=req.dependency
-                ))
-
-                self.install_q.put(InstallRequest(
-                    mod_info=mod_info,
-                    dependency=req.dependency
-                ))
-
-                if mod_info.file.mod_dependencies:
-                    print('Detected dependencies. ', end='')
-
-                    for dep in mod_info.file.mod_dependencies:
-                        self.queue_fetch(FetchRequest(
-                            mod_key=dep,
-                            mc_version=req.mc_version,
-                            channel=req.channel,
-                            dependency=True
-                        ))
-
-            print('')
+            self.do_fetch(req, carrot, args)
 
         print('Mod check phase complete. Proceeding to download...')
 
         while not self.download_q.empty():
             req = self.download_q.get()
-            print(f'Downloading file {colorify(req.mod_info.file.file_name, RED)} from {colorify(req.mod_info.file.download_url, BLUE)}...')
-            file_contents = self.backend.download_file(req.mod_info.file.download_url)
-            self.put_file_in_cache(file_contents, req.mod_info.file.file_name)
+            self.do_download(req, carrot, args)
 
         print('Download phase complete. Proceeding to installation...')
 
         while not self.install_q.empty():
             req = self.install_q.get()
-
-            current_mod = find_mod_by_key(carrot.mods, req.mod_info.key)
-            new_mod = InstalledModModel.from_dict(req.mod_info.to_dict())
-            new_mod.dependency = req.dependency
-
-            if not current_mod:
-                print(f'Installing mod {colorify(req.mod_info.name, WHITE + BRIGHT)} with file {colorify(req.mod_info.file.file_name, RED)}...')
-                carrot.mods.append(new_mod)
-
-                self.move_file_from_cache_to_content(new_mod.file.file_name)
-
-            else:
-                print(f'Updating mod {colorify(req.mod_info.name, WHITE + BRIGHT)} with file {colorify(req.mod_info.file.file_name, RED)}...')
-
-                # Prevent a user-installed mod from becoming a dependency
-                if not current_mod.dependency and new_mod.dependency:
-                    new_mod.dependency = False
-
-                replace_mod_by_key(carrot.mods, req.mod_info.key, new_mod)
-
-                self.delete_file(current_mod.file.file_name)
-                self.move_file_from_cache_to_content(req.mod_info.file.file_name)
+            self.do_install(req, carrot, args)
 
         print('Installation phase complete.')
+
+    def do_fetch(self, req: FetchRequest, carrot: CarrotModel, args):
+        print(f'Checking mod {colorify(str(req.mod_key), WHITE + BRIGHT)}...', end=' ')
+
+        mod_info = self.backend.get_mod_info(req.mod_key)
+
+        print(colorify(mod_info.name + '...', WHITE + BRIGHT), end=' ')
+
+        mod_info.file = self.backend.get_newest_file_info(req.mod_key, req.mc_version, req.channel)
+
+        current_mod = find_mod_by_key(carrot.mods, mod_info.key)
+
+        # TODO: Handling of "sticky" channel
+
+        if not current_mod:
+            print('New mod.', end=' ')
+
+            proceed = True
+
+        elif current_mod.file.id < mod_info.file.id:
+            if args.upgrade:
+                print('Mod already installed, found new version and will upgrade because it\'s allowed.',
+                      end=' ')
+
+                proceed = True
+
+            else:
+                print(f'A newer file was found but upgrades are disabled by default. Use the {colorify("--upgrade", RED + BRIGHT)} option if this should be allowed.',
+                      end=' ')
+
+                if req.dependency:
+                    print(f'\n{colorify("NOTE", RED + BRIGHT)}: Because this is a dependency, it will {colorify("not", BRIGHT)} be re-checked if you re-run last install command. Use "{colorify("carrot update " + mod_info.key, YELLOW + BRIGHT)}" to do it explicitly.',
+                          end=' ')
+
+                proceed = False
+
+        elif current_mod.file.id == mod_info.file.id:
+            print('Already at newest version.',
+                  end=' ')
+
+            proceed = False
+
+        else:
+            if args.downgrade:
+                print('Mod already installed, found older version and will downgrade because it\'s allowed.',
+                      end=' ')
+
+                proceed = True
+            else:
+                print(f'An older file was found but downgrades are disabled by default. Use the {colorify("--downgrade", RED + BRIGHT)} option if this was intended.',
+                      end=' ')
+
+                proceed = False
+
+        if proceed:
+            self.download_q.put(DownloadRequest(
+                mod_info=mod_info,
+                dependency=req.dependency
+            ))
+
+            self.install_q.put(InstallRequest(
+                mod_info=mod_info,
+                dependency=req.dependency
+            ))
+
+            if mod_info.file.mod_dependencies:
+                print('Detected dependencies.', end=' ')
+
+                for dep in mod_info.file.mod_dependencies:
+                    self.queue_fetch(FetchRequest(
+                        mod_key=dep,
+                        mc_version=req.mc_version,
+                        channel=req.channel,
+                        dependency=True
+                    ))
+
+        print('')
+
+    def do_download(self, req: DownloadRequest, carrot: CarrotModel, args):
+        print(f'Downloading file {colorify(req.mod_info.file.file_name, RED)} from {colorify(req.mod_info.file.download_url, BLUE)}...')
+        file_contents = self.backend.download_file(req.mod_info.file.download_url)
+        self.put_file_in_cache(file_contents, req.mod_info.file.file_name)
+
+    def do_install(self, req: InstallRequest, carrot: CarrotModel, args):
+        current_mod = find_mod_by_key(carrot.mods, req.mod_info.key)
+        new_mod = InstalledModModel.from_dict(req.mod_info.to_dict())
+        new_mod.dependency = req.dependency
+
+        if not current_mod:
+            print(f'Installing mod {colorify(req.mod_info.name, WHITE + BRIGHT)} with file {colorify(req.mod_info.file.file_name, RED)}...')
+            carrot.mods.append(new_mod)
+
+            self.move_file_from_cache_to_content(new_mod.file.file_name)
+
+        else:
+            print(f'Updating mod {colorify(req.mod_info.name, WHITE + BRIGHT)} with file {colorify(req.mod_info.file.file_name, RED)}...')
+
+            # Prevent a user-installed mod from becoming a dependency
+            if not current_mod.dependency and new_mod.dependency:
+                new_mod.dependency = False
+
+            replace_mod_by_key(carrot.mods, req.mod_info.key, new_mod)
+
+            self.delete_file(current_mod.file.file_name)
+            self.move_file_from_cache_to_content(req.mod_info.file.file_name)
 
     def put_file_in_cache(self, content: bytes, file_name: str):
         if not os.path.exists('.carrot_cache'):
